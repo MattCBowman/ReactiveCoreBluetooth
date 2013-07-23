ea3d2c7f5ed133101a752dee138ca0930e052402
Matt Bowman <mbowman@citrrus.com>
2013-07-23 13:32:23 -0400

    Initial Commit

---
+++ BluetoothTestApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BluetoothTestApp
+//
+//  Created by Matt Bowman on 7/23/13.
+//  Copyright (c) 2013 Citrrus, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
